e0f2b603d9a2cd25d1e16a39a16905bd4b0cf5b2
jian玄冰 <jianxuanhuo1@126.com>
2019-06-11 17:28:18 +0800

    初始化项目

---
+++ src/Bing.Geetest/Configs/IGeetestConfigProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Bing.Geetest.Configs
+{
+    /// <summary>
+    /// 极验配置提供器
+    /// </summary>
+    public interface IGeetestConfigProvider
+    {
+        /// <summary>
+        /// 获取配置
+        /// </summary>
+        GeetestConfig GetConfig();
+
+        /// <summary>
+        /// 获取配置
+        /// </summary>
+        Task<GeetestConfig> GetConfigAsync();
+    }
+}
---
+++ src/Bing.Geetest/Const.cs
@@ -0,0 +1,28 @@
+﻿namespace Bing.Geetest
+{
+    /// <summary>
+    /// 常量
+    /// </summary>
+    internal class Const
+    {
+        /// <summary>
+        /// SDK版本号
+        /// </summary>
+        public const string Version = "3.2.0";
+
+        /// <summary>
+        /// 极验验证API URL
+        /// </summary>
+        public const string ApiUrl = "https://api.geetest.com";
+
+        /// <summary>
+        /// 注册URL
+        /// </summary>
+        public const string RegisterUrl = "/register.php";
+
+        /// <summary>
+        /// 验证URL
+        /// </summary>
+        public const string ValidateUrl = "/validate.php";
+    }
+}
